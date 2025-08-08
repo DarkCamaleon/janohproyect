e2f79e1a9c92233e378b96b228b36fd2c769056b
alejandro <avenegasc@unap.cl>
2025-08-08 12:34:26 -0400

    componentes

---
+++ src/app/login/registrar/registrar.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registrar',
+  imports: [],
+  templateUrl: './registrar.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class RegistrarComponent { }
